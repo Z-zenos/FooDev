189c95620c1e8042c3bd61c13bec94f4dbdabcaa
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-19 00:28:14 +0700

    Add data for sample food

--- frontend/src/data.ts
+++ frontend/src/data.ts
@@ -2,7 +2,7 @@ import {Food} from "./app/share/models/food.model";
 
 export const sample_food: Food[] = [
   {
-    id:'1',
+    id: '1',
     name: 'Pizza Pepperoni',
     cookTime: '10-20',
     price: 10,
@@ -13,7 +13,7 @@ export const sample_food: Food[] = [
     tags: ['FastFood', 'Pizza', 'Lunch'],
   },
   {
-    id:'2',
+    id: '2',
     name: 'Meatball',
     price: 20,
     cookTime: '20-30',
@@ -24,7 +24,7 @@ export const sample_food: Food[] = [
     tags: ['SlowFood', 'Lunch'],
   },
   {
-    id:'3',
+    id: '3',
     name: 'Hamburger',
     price: 5,
     cookTime: '10-15',
@@ -35,7 +35,7 @@ export const sample_food: Food[] = [
     tags: ['FastFood', 'Hamburger'],
   },
   {
-    id:'4',
+    id: '4',
     name: 'Fried Potatoes',
     price: 2,
     cookTime: '15-20',
@@ -46,7 +46,7 @@ export const sample_food: Food[] = [
     tags: ['FastFood', 'Fry'],
   },
   {
-    id:'5',
+    id: '5',
     name: 'Chicken Soup',
     price: 11,
     cookTime: '40-50',
@@ -57,7 +57,7 @@ export const sample_food: Food[] = [
     tags: ['SlowFood', 'Soup'],
   },
   {
-    id:'6',
+    id: '6',
     name: 'Vegetables Pizza',
     price: 9,
     cookTime: '40-50',
