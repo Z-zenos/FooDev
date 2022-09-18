a85adb323ffc3ae84d13d05161e76d4a44888875
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-18 23:55:14 +0700

    Food model

---
+++ frontend/src/app/share/models/food.model.ts
@@ -0,0 +1,15 @@
+export class Food {
+  /*
+    !: required
+    ?: optional
+  */
+  id!: string;
+  name!: string;
+  price!: number;
+  tag?: string[];
+  favorite!: boolean;
+  stars!: number;
+  imageUrl!: string;
+  origins!: string[];
+  cookTime!: string;
+}
