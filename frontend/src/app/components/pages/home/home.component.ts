1d2951b46ffe1fae8ab34dcd940cbd244a6ca279
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-20 00:59:44 +0700

    Change UI by searchTerm and tag

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Food} from "../../../share/models/food.model";
 import {FoodService} from "../../../services/food.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,25 @@ import {FoodService} from "../../../services/food.service";
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
 
-  constructor(private _foodService: FoodService) {
-    this.foods = this._foodService.getAllFoods();
+  constructor(
+    private _foodService: FoodService,
+    private _activatedRoute: ActivatedRoute
+  ) {
+    this._activatedRoute.params.subscribe((params: Params) => {
+      const searchTerm = params['searchTerm'];
+      const tag = params['tag'];
+
+      // params.searchTerm -> Error: property 'searchTerm' comes from an index signature... -> Use params['searchTerm'] or disable this error from tsconfig.json: noPropertyAccessFromIndexSignature: false
+      if(searchTerm) {
+        this.foods = this._foodService.getAllFoodsBySearch(searchTerm);
+      }
+      else if(tag) {
+        this.foods = this._foodService.getAllFoodsByTag(tag);
+      }
+      else {
+        this.foods = this._foodService.getAllFoods();
+      }
+    });
   }
 
   ngOnInit(): void {
