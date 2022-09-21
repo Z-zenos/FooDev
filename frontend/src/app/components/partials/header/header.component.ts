eb1528a5a24d04da859a3e5ca4f5085196251a06
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-21 14:42:27 +0700

    Add cartQuantity prop, inject cartService

--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../../services/cart.service";
+import {Cart} from "../../../share/models/cart.model";
 
 @Component({
   selector: 'app-header',
@@ -6,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  cartQuantity = 0;
+  constructor(private _cartService: CartService) {
+    this._cartService.getCartObservable().subscribe((newCart: Cart) => {
+      this.cartQuantity = newCart.totalCount;
+    });
+  }
 
   ngOnInit(): void {
   }
