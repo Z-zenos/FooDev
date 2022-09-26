b971170538a67f38c753d7b8d1e0ae9b0a7af534
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-26 22:16:33 +0700

    Convert functions get data static to functions get data from backend

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,40 +1,54 @@
 import { Injectable } from '@angular/core';
 import {Food} from "../share/models/food.model";
-import {sample_food, sample_tags} from "../../data";
 import {Tag} from "../share/models/tag.model";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {
+  FOOD_BY_ID_URL,
+  FOODS_BY_SEARCH_URL,
+  FOODS_BY_TAG_URL,
+  FOODS_TAGS_URL,
+  FOODS_URL
+} from "../share/constants/urls";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
 
-  // Get all the foods from data.ts file
-  // In the future, this feature should be connected to the backend server and get data from mongoDB
-  getAllFoods(): Food[] {
-    return sample_food;
+  /*
+    http doesn't send the raw data as Food type, it will send
+    an observable, and we need to subscribe to it, after the
+    connection to the backend complete, it will return us our data
+    or an error
+  */
+  getAllFoods(): Observable<Food[]> {
+    return this._http.get<Food[]>(FOODS_URL);
   }
 
-  getAllFoodsBySearch(search: string) {
-    return this.getAllFoods().filter(f => f.name.toLowerCase().includes(search.toLowerCase()));
+  getAllFoodsBySearch(search: string): Observable<Food[]> {
+    return this._http.get<Food[]>(FOODS_BY_SEARCH_URL + search);
   }
 
-  getAllTags(): Tag[] {
-    return sample_tags;
+  getAllTags(): Observable<Tag[]> {
+    return this._http.get<Tag[]>(FOODS_TAGS_URL);
   }
 
-  getAllFoodsByTag(tag: string): Food[] {
+  getAllFoodsByTag(tag: string): Observable<Food[]> {
     return tag === 'All' ?
       this.getAllFoods() :
-      this.getAllFoods().filter(f => f.tags?.includes(tag));
+      this._http.get<Food[]>(FOODS_BY_TAG_URL + tag);
   }
 
-  getFoodById(foodId: string): Food {
+  getFoodById(foodId: string): Observable<Food> {
     /*
       find method could return an undefined
       -> Use coalescing operator (??) when previous part is undefined
     */
-    return this.getAllFoods().find(f => f.id === foodId) ?? new Food();
+    // return this.getAllFoods().find(f => f.id === foodId) ?? new Food();
+
+    return this._http.get<Food>(FOOD_BY_ID_URL + foodId);
   }
 }
