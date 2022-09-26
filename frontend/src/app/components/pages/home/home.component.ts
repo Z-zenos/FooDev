bebd631788abdc68cc9a42e009ac16d98fa36023
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-09-26 22:19:49 +0700

    Convert functions from get foods from static file to the backend server

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Food} from "../../../share/models/food.model";
 import {FoodService} from "../../../services/food.service";
 import {ActivatedRoute, Params} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-home',
@@ -15,20 +16,33 @@ export class HomeComponent implements OnInit {
     private _foodService: FoodService,
     private _activatedRoute: ActivatedRoute
   ) {
+    /*
+      Get the observables of these values then we need to subscribe
+      to it and set this.foods
+      If we don't use this variable, we need to subscribe to each
+      of these items separately that makes the code a little messy
+    */
+    let foodsObservable: Observable<Food[]>;
+
     this._activatedRoute.params.subscribe((params: Params) => {
       const searchTerm = params['searchTerm'];
       const tag = params['tag'];
 
       // params.searchTerm -> Error: property 'searchTerm' comes from an index signature... -> Use params['searchTerm'] or disable this error from tsconfig.json: noPropertyAccessFromIndexSignature: false
       if(searchTerm) {
-        this.foods = this._foodService.getAllFoodsBySearch(searchTerm);
+        foodsObservable = this._foodService.getAllFoodsBySearch(searchTerm);
       }
       else if(tag) {
-        this.foods = this._foodService.getAllFoodsByTag(tag);
+        foodsObservable = this._foodService.getAllFoodsByTag(tag);
       }
       else {
-        this.foods = this._foodService.getAllFoods();
+        foodsObservable = this._foodService.getAllFoods();
       }
+      foodsObservable.subscribe(foods => {
+        // @ts-ignore
+        this.foods = foods['data'].foods;
+      });
+
     });
   }
 
